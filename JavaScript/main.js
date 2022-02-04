d7acde33fde76d303236ae27e8ff05137e59362f
Vishwajeetsinh <rathodvishwajeet64@gmail.com>
2022-02-04 21:32:52 +0530

    neext dimension

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,109 +1,156 @@
+const loader = new THREE.TextureLoader()
+const cross = loader.load('./textures/cross.png')
+
+
+
+// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// curson position
-const cursor = {
-    x:0,
-    y:0
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+
+const particlesGeometry = new THREE.BufferGeometry;
+const particlesCnt = 5000;
+
+const posArray = new Float32Array(particlesCnt * 3);
+for (let i = 0; i<particlesCnt * 3 ; i++){
+    // posArray[i] = Math.random()
+    // posArray[i] = Math.random() -0.5
+    // posArray[i] = (Math.random() -0.5) * 5
+    posArray[i] = (Math.random() -0.5) * (Math.random() * 5)
 }
 
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3 ))
 
 
-// swindos size creat 
+// Materials
+
+const material = new THREE.PointsMaterial({
+    
+    size: 0.005,
+    color:'red'
+    
+
+})
+const particlesMaterial = new THREE.PointsMaterial({
+    
+    size: 0.005,
+    // map:cross,
+    color:'blue',
+    transparent: true,
+    blending: THREE.AdditiveBlending    
+})
+
+
+// material.color = new THREE.Color(0xff0000)
+
+// Mesh
+const sphere = new THREE.Points(geometry,material)
+
+const particlesMesh = new THREE.Points(particlesGeometry  ,particlesMaterial)
+
+scene.add(sphere, particlesMesh)
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
 const sizes = {
-    width: window.innerWidth,   
+    width: window.innerWidth,
     height: window.innerHeight
 }
 
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-// scene 
-const scene  = new THREE.Scene()
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
-
-// object
-const geometry = new THREE.TorusGeometry(1,.3,30,9)
-const material = new THREE.MeshBasicMaterial()
-
-
-material.color = new THREE.Color(0xffff00)
-
-
-const cube = new THREE.Mesh(geometry, material)
-
-scene.add(cube)
-
-
-
-
-// camera
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
 scene.add(camera)
 
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
 
-//renderes
-
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(new THREE.Color('#21282a'),1)
 
-let time = Date.now()
+// mouse
+document.addEventListener('mousemove', animateParticles)
 
-// clock second method for accurate rotation
+let mouseX = 0
+let mouseY = 0
+function animateParticles(event){
+    mouseY = event.clientY
+    mouseX = event.clientX
+}
+
+
+
+/**
+ * Animate
+ */
+
 const clock = new THREE.Clock()
 
-
-// usE  G S A P
-
-    // gsap.to(cube.position, {duration:1, delay:1, x:1})
-    // gsap.to(cube.position, {duration:1 , delay:2, x:0})
-
-
-const tick = () => 
-
+const tick = () =>
 {
 
-    let one = 1
-    let two = 2
+    const elapsedTime = clock.getElapsedTime()
 
-    const elaspedTime = clock.getElapsedTime()
+    // Update objects
+    sphere.rotation.y = .5 * elapsedTime
+    particlesMesh.rotation.y = -.1 * elapsedTime
 
-
-    // time 
-    const currentTime = Date.now()
-    const deltaTime = currentTime - time
-    time = currentTime
-    console.log(deltaTime)
-
-    cube.rotation.y = elaspedTime * Math.PI * .20
-    cube.rotation.z = elaspedTime * Math.PI * .20
+    if (mouseX > 0){
+    particlesMesh.rotation.x = -mouseX     * (elapsedTime * 0.00008)
+     particlesMesh.rotation.y = -mouseY     * (elapsedTime * 0.00008)
+    }
 
     
-    
-    window.addEventListener('mousemove', event => 
-    {
-        cursor.x = event.clientX / sizes.width - 0.5, 
-        cursor.y = event.clientY / sizes.height - 0.5
-    })
-    
-    // update camera
 
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * -5
-    camera.lookAt(cube.position)
-    
-    // cube.position.y = cursor.y * 3
+    // Update Orbital Controls
+    // controls.update()
 
-
+    // Render
     renderer.render(scene, camera)
 
-    console.log('tick')
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
-
-
 }
+
 tick()
