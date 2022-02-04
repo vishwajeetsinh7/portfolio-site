776bfbac6835530dd0a9389da9820b959bfffe00
Vishwajeetsinh <rathodvishwajeet64@gmail.com>
2022-02-04 14:42:28 +0530

    extra things

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,26 +1,109 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const canvas = document.querySelector('canvas.webgl')
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 6;
-
-renderer.render(scene, camera);
-
-
-function animate() {
-	requestAnimationFrame( animate ); 
-
-    cube.rotation.y += 0.01;
-    cube.rotation.x += 0.01; 
-    cube.rotation.z += 0.01;   
-	renderer.render( scene, camera );
+// curson position
+const cursor = {
+    x:0,
+    y:0
 }
-animate();
+
+
+
+// swindos size creat 
+const sizes = {
+    width: window.innerWidth,   
+    height: window.innerHeight
+}
+
+
+
+// scene 
+const scene  = new THREE.Scene()
+
+
+// object
+const geometry = new THREE.TorusGeometry(1,.3,30,9)
+const material = new THREE.MeshBasicMaterial()
+
+
+material.color = new THREE.Color(0xffff00)
+
+
+const cube = new THREE.Mesh(geometry, material)
+
+scene.add(cube)
+
+
+
+
+// camera
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+
+//renderes
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+
+let time = Date.now()
+
+// clock second method for accurate rotation
+const clock = new THREE.Clock()
+
+
+// usE  G S A P
+
+    // gsap.to(cube.position, {duration:1, delay:1, x:1})
+    // gsap.to(cube.position, {duration:1 , delay:2, x:0})
+
+
+const tick = () => 
+
+{
+
+    let one = 1
+    let two = 2
+
+    const elaspedTime = clock.getElapsedTime()
+
+
+    // time 
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+    console.log(deltaTime)
+
+    cube.rotation.y = elaspedTime * Math.PI * .20
+    cube.rotation.z = elaspedTime * Math.PI * .20
+
+    
+    
+    window.addEventListener('mousemove', event => 
+    {
+        cursor.x = event.clientX / sizes.width - 0.5, 
+        cursor.y = event.clientY / sizes.height - 0.5
+    })
+    
+    // update camera
+
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    camera.position.y = cursor.y * -5
+    camera.lookAt(cube.position)
+    
+    // cube.position.y = cursor.y * 3
+
+
+    renderer.render(scene, camera)
+
+    console.log('tick')
+    window.requestAnimationFrame(tick)
+
+
+}
+tick()
